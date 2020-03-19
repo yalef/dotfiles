a3f3c126b60204e6b4a1c4120bd0c5690c13e41d
yalef <vlad1vaselkov@gmail.com>
2020-03-19 15:02:48 +0500

    new lock script

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
-set -e
-xset s off dpms 0 10 0
-i3lock --color=4c7899 --ignore-empty-password --show-failed-attempts --nofork
-xset s off -dpms
+#!/bin/sh -e
+
+# Take a screenshot
+scrot /home/yalef/scrot.png
+
+# Pixellate 10x
+# or blur
+mogrify -blur 0x4 /home/yalef/scrot.png   
+#-scale 10% -scale 1050% /root/Desktop/pictures/lock_scrot/scrot.png
+
+# Lock screen displaying this image
+i3lock -i /home/yalef/scrot.png --ignore-empty-password
