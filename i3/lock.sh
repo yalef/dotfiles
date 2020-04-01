bcc8ca8ce7984520cf2205a167ec508fe9b60342
yalef <vlad1vaselkov@gmail.com>
2020-04-01 15:19:29 +0500

    font changed

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,13 +1,12 @@
 #!/bin/sh -e
 
 # Take a screenshot
+rm -f /home/yalef/scrot*
 scrot /home/yalef/scrot.png
 
 # Pixellate 10x
 # or blur
-mogrify -blur 0x4 /home/yalef/scrot.png   
-#-scale 10% -scale 1050% /root/Desktop/pictures/lock_scrot/scrot.png
-
+#mogrify -blur 0x4 /home/yalef/scrot.png   
+mogrify -scale 10% -scale 1000% /home/yalef/scrot.png
 # Lock screen displaying this image
-i3lock -i /home/yalef/scrot.png --ignore-empty-password
-rm -f /home/yalef/scrot*.png
+i3lock -i /home/yalef/scrot.png --ignore-empty-password 
