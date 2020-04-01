4b42e98197122a3ca82927b42093bef4974b51be
yalef <vlad1vaselkov@gmail.com>
2020-04-01 16:17:49 +0500

    font changed and lock script

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,12 +1,15 @@
 #!/bin/sh -e
 
 # Take a screenshot
-rm -f /home/yalef/scrot*
 scrot /home/yalef/scrot.png
-
 # Pixellate 10x
 # or blur
 #mogrify -blur 0x4 /home/yalef/scrot.png   
 mogrify -scale 10% -scale 1000% /home/yalef/scrot.png
 # Lock screen displaying this image
+set -e
+xset s off dpms 0 10 0
 i3lock -i /home/yalef/scrot.png --ignore-empty-password 
+xset s off -dpms
+# remove screenshot
+rm -f /home/yalef/scrot*
