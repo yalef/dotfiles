4436040e352fb19ca1d952df4a9571665eff7da0
yalef <vlad1vaselkov@gmail.com>
2020-04-02 14:48:13 +0500

    changed lock script

--- i3/lock.sh
+++ i3/lock.sh
@@ -8,8 +8,8 @@ scrot /home/yalef/scrot.png
 mogrify -scale 10% -scale 1000% /home/yalef/scrot.png
 # Lock screen displaying this image
 set -e
-xset s off dpms 0 10 0
-i3lock -i /home/yalef/scrot.png --ignore-empty-password 
+i3lock -i /home/yalef/scrot.png --ignore-empty-password
+xset s off dpms 0 10 0 
 xset s off -dpms
 # remove screenshot
 rm -f /home/yalef/scrot*
