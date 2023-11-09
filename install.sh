e722eb81f6d4ee34e2696f9e46de48f705c522de
Vladislav <vladislav.romaschenko@saritasa.com>
2023-11-09 14:04:11 +0500

    add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CURRENT_DIR=$(pwd)
+
+# create .config if it's not exist
+if ! [ -d $HOME/.config/ ]; then
+  mkdir $HOME/.config/
+fi
+ 
+# # move all content from current dir to config
+mv * $HOME/.config/
+cd $HOME/.config/
+
+# rm repo dir
+rm -r "$CURRENT_DIR"
