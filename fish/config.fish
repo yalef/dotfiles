dae49057c2102ecb648666f09f2e5b6376cc380e
Vladislav Romashchenko <vlad1vaselkov@gmail.com>
2026-04-17 11:03:26 +0500

    fish: set nvim as default editor

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,10 @@ if status is-interactive
 end
 
 set fish_greeting
-set EDITOR hx
-set NNN_OPENER hx
-set PATH $PATH:$HOME/.local/bin
+set EDITOR nvim
+set PATH $PATH:$HOME/.local/bin:$HOME/Projects/lua-language-server/bin
+set TERM st-256color
+
 alias ls="ls -AC --color --group-directories-first"
-alias nnn="nnn -H -c -d"
+alias nnn="nnn -H -d -A -e"
+alias vim="nvim"
